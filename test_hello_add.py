924166742c96262bbf77fcdb0639527c886bae30
ssebastien <43267379+ssebastien@users.noreply.github.com>
2020-06-11 14:56:03 +0200

    Create test_hello_add.py

---
+++ test_hello_add.py
@@ -0,0 +1,14 @@
+from hello_add import app
+from flask import json
+
+def test_add():        
+    response = app.test_client().post(
+        '/add',
+        data=json.dumps({'a': 1, 'b': 2}),
+        content_type='application/json',
+    )
+
+    data = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 200
+    assert data['sum'] == 3

c9d9a8c953f03c666c10a211e5206b3cef772f0c
ssebastien <43267379+ssebastien@users.noreply.github.com>
2020-06-11 14:54:01 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,2 +1,10 @@
+from flask import Flask, request, jsonify
 
-print("hello")
+app = Flask(__name__)
+
+@app.route('/add', methods=['POST'])
+def add():
+    data = request.get_json()
+    return jsonify({'sum': data['a'] + data['b']})
+
+#print("hello")

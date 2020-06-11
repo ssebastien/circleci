76f9691b5b39fd9b68c0b0c680cee0f056482cba
ssebastien <43267379+ssebastien@users.noreply.github.com>
2020-06-11 17:00:14 +0200

    Update test_hello_add.py

--- test_hello_add.py
+++ test_hello_add.py
@@ -7,8 +7,7 @@ def test_add():
         data=json.dumps({'a': 1, 'b': 2}),
         content_type='application/json',
     )
+data = json.loads(response.get_data(as_text=True))
 
-    data = json.loads(response.get_data(as_text=True))
-
-    assert response.status_code == 200
-    assert data['sum'] == 3
+assert response.status_code == 200
+assert data['sum'] == 3

5f85b660f084446fce1acb7e5c46430384d32a22
ssebastien <sebastien.spillemaecker@gmail.com>
2020-09-29 15:03:14 +0200

    commit depuis pycharm

--- src/server.py
+++ src/server.py
@@ -3,7 +3,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-        return "Hello"
+        return "Hello 1"
 
 if __name__ == "__main__":
         server.run(debug=True,host='0.0.0.0', port=5001)
